4bc2c90abe96bb6305688b5bf42b6c0454160178
zhenzhi <wuzhenzhi@gmail.com>
2025-12-21 21:09:32 +0800

    version v1.1 setup

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="agiagent",
-    version="1.0.2",
+    version="1.1",
     author=f"{APP_NAME} Team",
     description="AGI Agent for general purposed task execution",
     long_description=long_description,
